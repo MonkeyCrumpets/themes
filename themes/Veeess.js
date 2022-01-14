a6015606b384c63424c275d76845c00009627789
MonkeyCrumpets <brett@kluske.com>
2022-01-15 06:49:43 +1100

    feat: create Veeess.js

---
+++ themes/Veeess.js
@@ -0,0 +1,39 @@
+black = "#000000";
+red = "#CD3131"; // red
+green = "#0dbc51"; // green
+yellow = "#e5e510"; // yellow
+blue = "#2482c8"; // blue
+magenta = "#bc3fbc"; // pink
+cyan = "#11a8cd"; // cyan
+white = "#e5e5e5"; // light gray
+lightBlack = "#666666"; // medium gray
+lightRed = "#f14c4c"; // red
+lightGreen = "#23d159"; // green
+lightYellow = "#f5f543"; // yellow
+lightBlue = "#3b8eea"; // blue
+lightMagenta = "#d670d6"; // pink
+lightCyan = "#29b8db"; // cyan
+lightWhite = "#e5e5e5"; // white
+
+t.prefs_.set("color-palette-overrides", [
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+  lightBlack,
+  lightRed,
+  lightGreen,
+  lightYellow,
+  lightBlue,
+  lightMagenta,
+  lightCyan,
+  lightWhite,
+]);
+
+t.prefs_.set("cursor-color", "rgba(255, 255, 255, 0.5)");
+t.prefs_.set("foreground-color", "#ffffff");
+t.prefs_.set("background-color", "#1e1e1e");
